afaf1d4ecaf5e75f7f94dee84b73d9341acc8464
Jake VanderPlas <jakevdp@gmail.com>
2017-06-15 13:41:23 -0700

    Add JSON traitlets + tests

--- altair_parser/json_traitlets.py
+++ altair_parser/json_traitlets.py
@@ -1,20 +1,56 @@
 """
 Extensions to traitlets for use with JSON
 """
-
-# Do this so that we can import this module in place of traitlets
-from traitlets import *
-
 import traitlets as T
 
 
-class Null(TraitType):
-    """A trait for complex numbers."""
+class UndefinedType(object):
+    """A Singleton type to mark undefined traits"""
+    __instance = None
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls.__instance, cls):
+            cls.__instance = object.__new__(cls, *args, **kwargs)
+        return cls.__instance
+    def __repr__(self):
+        return "undefined"
+undefined = UndefinedType()
 
-    default_value = None
-    info_text = 'a null value'
+
+class JSONNull(T.TraitType):
+    default_value = undefined
+    info_text = 'a JSON null value'
 
     def validate(self, obj, value):
-        if value is None:
+        if value is undefined or value is None:
             return value
         self.error(obj, value)
+
+
+class JSONNumber(T.CFloat):
+    default_value = undefined
+    info_text = "a JSON number"
+
+    def validate(self, obj, value):
+        if value is undefined:
+            return value
+        return super().validate(obj, value)
+
+
+class JSONString(T.Unicode):
+    default_value = undefined
+    info_text = "a JSON string"
+
+    def validate(self, obj, value):
+        if value is undefined:
+            return value
+        return super().validate(obj, value)
+
+
+class JSONBoolean(T.Bool):
+    default_value = undefined
+    info_text = "a JSON boolean"
+
+    def validate(self, obj, value):
+        if value is undefined:
+            return value
+        return super().validate(obj, value)
---
+++ altair_parser/tests/test_json_traitlets.py
@@ -0,0 +1,23 @@
+import pytest
+
+import traitlets as T
+
+from ..json_traitlets import (undefined, UndefinedType, JSONNull,
+                              JSONNumber, JSONString, JSONBoolean)
+
+
+def test_undefined_singleton():
+    assert UndefinedType() is undefined
+
+
+@pytest.mark.parametrize('Trait,failcase,passcase',
+                         [(JSONNull, 42, None), (JSONBoolean, 'abc', True),
+                          (JSONNumber, None, 42), (JSONString, 50, 'abc')])
+def test_traits(Trait, failcase, passcase):
+    trait = Trait()
+    obj = None
+
+    trait.validate(obj, passcase)
+    with pytest.raises(T.TraitError) as err:
+        trait.validate(obj, failcase)
+    assert trait.info_text in str(err)
