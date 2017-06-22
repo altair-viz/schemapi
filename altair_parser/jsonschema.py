257815e86b3ef3ed6494afc4583c230b17bbf713
Jake VanderPlas <jakevdp@uw.edu>
2017-06-22 08:54:15 -0700

    Initial refactor trait code logic

--- altair_parser/jsonschema.py
+++ altair_parser/jsonschema.py
@@ -27,6 +27,159 @@ class {{ cls.classname }}({{ cls.baseclass }}):
 '''
 
 
+class TraitCodeExtractor(object):
+    """Base class for trait code extractors.
+
+    An ordered list of these is passed to JSONSchema, and they are used to
+    extract appropriate trait codes.
+    """
+    #def __init__(self, schema, typecode=None):
+    #    self.schema = schema
+    #    self.typecode = typecode or schema.type
+
+    def check(self):
+        raise NotImplementedError()
+
+    def trait_code(self, **kwargs):
+        raise NotImplementedError()
+
+class SimpleTraitCode(TraitCodeExtractor):
+    simple_types = ["boolean", "null", "number", "integer", "string"]
+    classes = {'boolean': 'jst.JSONBoolean',
+               'null': 'jst.JSONNull',
+               'number': 'jst.JSONNumber',
+               'integer': 'jst.JSONInteger',
+               'string': 'jst.JSONString'}
+    validation_keys = {'number': ['minimum', 'exclusiveMinimum',
+                                  'maximum', 'exclusiveMaximum',
+                                  'multipleOf'],
+                       'integer': ['minimum', 'exclusiveMinimum',
+                                   'maximum', 'exclusiveMaximum',
+                                   'multipleOf']}
+    def check(self, schema, typecode=None):
+        typecode = typecode or schema.type
+        return typecode in self.simple_types
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        typecode = typecode or schema.type
+        cls = self.classes[typecode]
+        keys = self.validation_keys.get(typecode, [])
+        kwargs.update({key: schema[key] for key in keys if key in schema})
+        return construct_function_call(cls, **kwargs)
+
+
+class CompoundTraitCode(TraitCodeExtractor):
+    simple_types = SimpleTraitCode.simple_types
+    def check(self, schema, typecode=None):
+        typecode = typecode or schema.type
+        return isinstance(typecode, list)
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        typecode = typecode or schema.type
+        for typ in typecode:
+            assert typ in self.simple_types
+        if 'null' in typecode:
+            kwargs['allow_none'] = True
+        typecode = [typ for typ in typecode if typ != 'null']
+        simple = SimpleTraitCode()
+        if len(typecode) == 1:
+            return simple.trait_code(schema, typecode[0], **kwargs)
+        else:
+            item_kwargs = {key:val for key, val in kwargs.items()
+                           if key not in ['allow_none', 'allow_undefined']}
+            arg = "[{0}]".format(', '.join(simple.trait_code(schema, typ, **item_kwargs)
+                                           for typ in typecode))
+            return construct_function_call('jst.JSONUnion', Variable(arg), **kwargs)
+
+
+class RefTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return '$ref' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        ref = schema.get_reference(schema['$ref'])
+        if ref.is_object:
+            return construct_function_call('jst.JSONInstance',
+                                           Variable(ref.classname),
+                                           **kwargs)
+        else:
+            ref = ref.copy()  # TODO: maybe can remove this?
+            ref.metadata = schema.metadata
+            return ref.trait_code
+
+
+class EnumTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return 'enum' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        return construct_function_call('jst.JSONEnum', schema["enum"], **kwargs)
+
+
+class ArrayTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return schema.type == 'array'
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        # TODO: implement items as list and additionalItems
+        items = schema['items']
+        if 'minItems' in schema:
+            kwargs['minlen'] = schema['minItems']
+        if 'maxItems' in schema:
+            kwargs['maxlen'] = schema['maxItems']
+        if 'uniqueItems' in schema:
+            kwargs['uniqueItems'] = schema['uniqueItems']
+        if isinstance(items, list):
+            raise NotImplementedError("'items' keyword as list")
+        else:
+            itemtype = schema.make_child(items).trait_code
+        return construct_function_call('jst.JSONArray', Variable(itemtype),
+                                       **kwargs)
+
+
+class AnyOfTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return 'anyOf' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        children = [Variable(schema.make_child(sub_schema).trait_code)
+                    for sub_schema in schema.schema['anyOf']]
+        return construct_function_call('jst.JSONAnyOf', Variable(children),
+                                       **kwargs)
+
+
+class OneOfTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return 'oneOf' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        children = [Variable(schema.make_child(sub_schema).trait_code)
+                    for sub_schema in schema.schema['oneOf']]
+        return construct_function_call('jst.JSONOneOf', Variable(children),
+                                       **kwargs)
+
+
+class AllOfTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return 'allOf' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        children = [Variable(schema.make_child(sub_schema).trait_code)
+                    for sub_schema in schema.schema['allOf']]
+        return construct_function_call('jst.JSONAllOf', Variable(children),
+                                       **kwargs)
+
+
+class NotTraitCode(TraitCodeExtractor):
+    def check(self, schema, typecode=None):
+        return 'not' in schema
+
+    def trait_code(self, schema, typecode=None, **kwargs):
+        not_this = schema.make_child(schema['not']).trait_code
+        return construct_function_call('jst.JSONNot', Variable(not_this),
+                                       **kwargs)
+
+
 class JSONSchema(object):
     """A class to wrap JSON Schema objects and reason about their contents"""
     object_template = OBJECT_TEMPLATE
@@ -71,6 +224,12 @@ class JSONSchema(object):
         # if context is not given, then assume this is a root instance that
         # defines its context
         self.context = context or schema
+
+    def __getitem__(self, key):
+        return self.schema[key]
+
+    def __contains__(self, key):
+        return key in self.schema
 
     def copy(self):
         return self.__class__(schema=self.schema, context=self.context,
@@ -190,98 +349,49 @@ class JSONSchema(object):
         return wrapped_schema
 
     def _simple_trait_code(self, typecode, kwargs):
-        assert typecode in self.simple_types
-
-        info = self.traitlet_map[typecode]
-        cls = info['cls']
-        args = info.get('args', ())
-        kwargs.update(info.get('kwargs', {}))
-        keys = info.get('validation_keys', ())
-        kwargs.update({key: self.schema[key]
-                       for key in keys
-                       if key in self.schema})
-        return construct_function_call(cls, *args, **kwargs)
+        validator = SimpleTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _compound_trait_code(self, typecode, kwargs):
-        assert isinstance(typecode, list)
-
-        for typ in typecode:
-            assert typ in self.simple_types
-        if 'null' in typecode:
-            kwargs['allow_none'] = True
-        typecode = [typ for typ in typecode if typ != 'null']
-        if len(typecode) == 1:
-            return self._simple_trait_code(typecode[0], kwargs)
-        else:
-            item_kwargs = {key:val for key, val in kwargs.items()
-                           if key not in ['allow_none', 'allow_undefined']}
-            arg = "[{0}]".format(', '.join(self._simple_trait_code(typ, item_kwargs)
-                                           for typ in typecode))
-            return construct_function_call('jst.JSONUnion', Variable(arg), **kwargs)
+        validator = CompoundTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _ref_trait_code(self, typecode, kwargs):
-        assert '$ref' in self.schema
-
-        ref = self.get_reference(self.schema['$ref'])
-        if ref.is_object:
-            return construct_function_call('jst.JSONInstance',
-                                           Variable(ref.classname),
-                                           **kwargs)
-        else:
-            ref = ref.copy()  # TODO: maybe can remove this?
-            ref.metadata = self.metadata
-            return ref.trait_code
+        validator = RefTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _enum_trait_code(self, typecode, kwargs):
-        assert 'enum' in self.schema
-        return construct_function_call('jst.JSONEnum', self.schema["enum"],
-                                       **kwargs)
+        validator = EnumTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _array_trait_code(self, typecode, kwargs):
-        assert typecode == 'array'
-        # TODO: fix items and implement additionalItems
-        items = self.schema['items']
-        if 'minItems' in self.schema:
-            kwargs['minlen'] = self.schema['minItems']
-        if 'maxItems' in self.schema:
-            kwargs['maxlen'] = self.schema['maxItems']
-        if 'uniqueItems' in self.schema:
-            kwargs['uniqueItems'] = self.schema['uniqueItems']
-        if isinstance(items, list):
-            # TODO: need to implement this in the JSONArray traitlet
-            # Also need to check value of "additionalItems"
-            raise NotImplementedError("'items' keyword as list")
-        else:
-            itemtype = self.make_child(items).trait_code
-        return construct_function_call('jst.JSONArray', Variable(itemtype),
-                                       **kwargs)
+        validator = ArrayTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _anyOf_trait_code(self, typecode, kwargs):
-        assert 'anyOf' in self.schema
-        children = [Variable(self.make_child(schema).trait_code)
-                    for schema in self.schema['anyOf']]
-        return construct_function_call('jst.JSONAnyOf', Variable(children),
-                                       **kwargs)
+        validator = AnyOfTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _allOf_trait_code(self, typecode, kwargs):
-        assert 'allOf' in self.schema
-        children = [Variable(self.make_child(schema).trait_code)
-                    for schema in self.schema['allOf']]
-        return construct_function_call('jst.JSONAllOf', Variable(children),
-                                       **kwargs)
+        validator = AllOfTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _oneOf_trait_code(self, typecode, kwargs):
-        assert 'oneOf' in self.schema
-        children = [Variable(self.make_child(schema).trait_code)
-                    for schema in self.schema['oneOf']]
-        return construct_function_call('jst.JSONOneOf', Variable(children),
-                                       **kwargs)
+        validator = OneOfTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     def _not_trait_code(self, typecode, kwargs):
-        assert 'not' in self.schema
-        not_this = self.make_child(self.schema['not']).trait_code
-        return construct_function_call('jst.JSONNot', Variable(not_this),
-                                       **kwargs)
+        validator = NotTraitCode()
+        assert validator.check(self, typecode=typecode)
+        return validator.trait_code(self, typecode=typecode, **kwargs)
 
     @property
     def trait_code(self):
