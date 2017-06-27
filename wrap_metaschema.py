5ca5d9e4351c5ec6a5ddf40a1893f8d147eb0b3d
Jake VanderPlas <jakevdp@gmail.com>
2017-06-27 11:30:04 -0700

    Add example scripts to wrap schemas

---
+++ wrap_metaschema.py
@@ -0,0 +1,21 @@
+"""
+Wrap the draft-4 metaschema and save the source tree
+"""
+import os
+
+from jsonschema._utils import load_schema
+from altair_parser import JSONSchema
+from altair_parser.utils.save_module import save_module
+
+schema = load_schema('draft4')
+module = '_metaschema'
+
+if os.path.exists(module):
+    raise ValueError("{module} already exists.".format(module=module))
+
+schema = JSONSchema(schema, module=module)
+source_tree = schema.source_tree()
+print("writing to {module}".format(module=module))
+save_module(source_tree, module, os.path.abspath('.'))
+
+
---
+++ wrap_vegalite_schema.py
@@ -0,0 +1,18 @@
+"""
+Wrap the altair schema and save to a source tree
+"""
+import os
+from altair_parser import JSONSchema
+from altair_parser.utils.save_module import save_module
+
+#schemafile = 'altair_parser/schemas/vega-v3.0.0.json'
+schemafile = 'altair_parser/schemas/vega-lite-v2.0.0.json'
+module = '_altair_schema'
+
+if os.path.exists(module):
+    raise ValueError("{module} already exists.".format(module=module))
+
+schema = JSONSchema.from_json_file(schemafile, module=module)
+source_tree = schema.source_tree()
+print("writing to {module}".format(module=module))
+save_module(source_tree, module, os.path.abspath('.'))
