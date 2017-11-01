06a3c94d9c11c203cfe1d9b315fceec2d337869d
Jake VanderPlas <jakevdp@gmail.com>
2017-11-01 13:41:29 -0700

    TST: add more JSONSchema tests

--- schemapi/tests/test_validators.py
+++ schemapi/tests/test_validators.py
@@ -114,3 +114,44 @@ def test_simple_validation(schema, valid, invalid):
     for value in invalid:
         with pytest.raises(SchemaValidationError):
             schemaobj.validate(value)
+
+
+@pytest.fixture
+def definition_schema():
+    return {
+        '$ref': '#/definitions/TopLevel',
+        'definitions': {
+            'TopLevel': {
+                'anyOf': [
+                    {'$ref': '#/definitions/Chart'},
+                    {'$ref': '#/definitions/LayeredChart'},
+                    {'$ref': '#/definitions/FacetedChart'}
+                ]
+            },
+            'Chart': {'type': 'object', 'properties': {'a': {'type': 'string'}}},
+            'LayeredChart': {'type': 'object', 'properties': {'a': {'type': 'integer'}}},
+            'FacetedChart': {'type': 'object', 'properties': {'a': {'type': 'number'}}},
+        }
+    }
+
+
+def test_definition_schema(definition_schema):
+    root = JSONSchema(definition_schema)
+    assert len(root._registry) == 11
+
+
+@pytest.fixture
+def circular_schema():
+    return{
+        '$ref': '#/definitions/Obj',
+        'definitions': {
+            'Obj': {'anyOf': [{'type': 'integer'},
+                              {'type': 'array', 'items': {'$ref': '#/definitions/Obj'}}]}
+        }
+    }
+
+
+def test_circular_schema(circular_schema):
+    root = JSONSchema(circular_schema)
+    assert isinstance(root.validators[0], val.RefValidator)
+    assert isinstance(root.children[0].validators[0], val.AnyOfValidator)
