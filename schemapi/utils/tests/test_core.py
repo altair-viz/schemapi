243ce73b3acee78276f8fe34c6dd76d7a6ab2d18
Jake VanderPlas <jakevdp@gmail.com>
2017-11-01 12:36:24 -0700

    Add new utils

--- schemapi/utils/tests/test_core.py
+++ schemapi/utils/tests/test_core.py
@@ -5,49 +5,6 @@ import pytest
 from .. import hash_schema, regularize_name, trait_name_map, sorted_repr
 
 
-def generate_schemas():
-    yield 4
-    yield 3.14
-    yield 'foo'
-    yield None
-    yield {'a': 1, 'b': 2}
-    yield [1, 2, 3]
-    yield {1, 2, 3}
-    yield (1, 2, 3)
-    yield {
-        'a': {
-            'key': [1, 2, 3],
-            'val': {'a', 'b', 'c'}
-            },
-        'b': [{}, {'name': 'bob', 'age': 31}],
-        'c': (1, 2, 3)
-    }
-
-
-def scramble(val):
-    if isinstance(val, dict):
-        L = [(k, scramble(v)) for k, v in val.items()]
-        random.shuffle(L)
-        return dict(L)
-    elif isinstance(val, set):
-        L = [scramble(v) for v in val]
-        random.shuffle(L)
-        return set(L)
-    elif isinstance(val, tuple):
-        return tuple(map(scramble, val))
-    elif isinstance(val, list):
-        return list(map(scramble, val))
-    else:
-        return val
-
-
-@pytest.mark.parametrize('schema', generate_schemas())
-def test_hash_schema(schema):
-    """Test that schemas compile correctly, even when order is changed"""
-    hsh = hash_schema(schema)
-    assert all(hash_schema(scramble(schema)) == hsh for i in range(10))
-
-
 def test_regularize_name():
     assert regularize_name('for') == 'for_'
     assert regularize_name('__foo') == '__foo'
@@ -87,3 +44,57 @@ def test_sorted_repr():
     reprD = sorted_repr(D)
     assert reprD == "{'A': 5, 'B': {'x': 3, 'y': 4}}"
     assert eval(reprD) == D
+
+
+def generate_schemas():
+    yield 4
+    yield 3.14
+    yield 'foo'
+    yield None
+    yield {'a': 1, 'b': 2}
+    yield [1, 2, 3]
+    yield (1, 2, 3)
+    yield {
+        'a': {
+            'key': [1, 2, 3],
+            'val': ['a', 'b', 'c']
+            },
+        'b': [{}, {'name': 'bob', 'age': 31}],
+        'c': (1, 2, 3)
+    }
+    yield {
+        '$ref': '#/definitions/TopLevel',
+        'definitions': {
+            'TopLevel': {
+                'anyOf': [
+                    {'$ref': '#/definitions/Chart'},
+                    {'$ref': '#/definitions/LayeredChart'},
+                    {'$ref': '#/definitions/FacetedChart'}
+                ]
+            },
+            'Chart': {'type': 'object', 'properties': {'a': {'type': 'string'}}},
+            'LayeredChart': {'type': 'object', 'properties': {'a': {'type': 'integer'}}},
+            'FacetedChart': {'type': 'object', 'properties': {'a': {'type': 'number'}}},
+        }
+    }
+
+
+def scramble(val):
+    # scramble the order in which the dict is defined
+    if isinstance(val, dict):
+        L = [(k, scramble(v)) for k, v in val.items()]
+        random.shuffle(L)
+        return dict(L)
+    elif isinstance(val, tuple):
+        return tuple(map(scramble, val))
+    elif isinstance(val, list):
+        return list(map(scramble, val))
+    else:
+        return val
+
+
+@pytest.mark.parametrize('schema', generate_schemas())
+def test_hash_schema(schema):
+    """Test that schemas compile correctly, even when order is changed"""
+    hsh = hash_schema(schema)
+    assert all(hash_schema(scramble(schema)) == hsh for i in range(10))
